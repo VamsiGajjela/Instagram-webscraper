2d99f2737531536ffab699843f66dd78115b3ff2
Vamsi Gajjela <43142056+VamsiGajjela@users.noreply.github.com>
2020-07-18 00:20:15 -0400

    Update insta_content_grabber.py

--- insta_content_grabber.py
+++ insta_content_grabber.py
@@ -1,20 +1,27 @@
 #Vamsi Madhav Gajjela
-from urllib.request import Request, urlopen, urlretrieve
-
 def return_link(url, content_type):
     """
-    Using a url and a content type the functionr returns the contents
+    Using a url and a content type the function returns the contents
     source link to be used as pleased
     """
     search_val = 'og:image'
+    final = None
 
     if content_type == 'video':
         search_val = 'og:video'
-    
-    for line in urlopen(url):
-        line = str(line)
-        if search_val in line:
-            return clean_data(line)
+    try:
+        for line in urlopen(url):
+            line = str(line)
+            if search_val in line:
+                final = clean_data(line)
+
+            elif 'is_private' in line:
+                clean1 = line.find('"is_private"') + len('"is_private"') - 1
+                clean2 = line[clean1:]
+                priv_check = clean2[2:clean2.find(',')]
+                return (True if priv_check == 'true' else False, final)
+    except:
+        return True, ''
 
 
 def clean_data(line):
@@ -48,13 +55,22 @@ if __name__ == '__main__':
             quit()
 
         else:
-            print("Seems like you didn't put in a proper type, try again!")
+            print("Seems like you didn't put in a proper type, try again!\n")
+            continue 
             
         url = input('Enter the link of the instagram post: ')
+        contents = return_link(url, content)
+
+        if contents[0]:
+            print('Sorry, This seems to be a private account or an invalid link try another one!\n' + '-'*80 + '\n')
+            continue
+        
         name = input('What would you like to name the file? ')
-        save_image(return_link(url, content), name + end)
+        
+        save_image(contents[1], name + end)
         print('The image/video has been saved locally!')
 
         print('-'*80 + '\n')
+    
     
             
